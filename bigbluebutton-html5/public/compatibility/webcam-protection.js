0aeba4ab364be4f43a6f870cb3b686825bf60df6
Daniel Kruse <ich@daniel-kruse.de>
2020-06-19 14:09:37 +0200

    added jquery js file to project

--- bigbluebutton-html5/public/compatibility/webcam-protection.js
+++ bigbluebutton-html5/public/compatibility/webcam-protection.js
@@ -1,8 +1,3 @@
-// Load jquery
-let script = document.createElement('script');
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(script);
-
 // Init
 attachMutationObserver();
 
